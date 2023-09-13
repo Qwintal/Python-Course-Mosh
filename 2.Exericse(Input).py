317765a0fb6d13ce6d3be640d6ff2c230336a9df
Qwintal <mr.ankituniyal@gmail.com>
2023-09-13 10:23:37 +0530

    1.Exericise(Variable) This exercise helps to understand variable.

---
+++ 2.Exericse(Input).py
@@ -0,0 +1,6 @@
+# Ask two questions: person's name and favourite color.Then, print a message like "mosh likes blue".
+Name = input("What is your name?")
+Favourite_color = input("What is your favourite color?")
+
+print(f"{Name} likes {Favourite_color}")
+
