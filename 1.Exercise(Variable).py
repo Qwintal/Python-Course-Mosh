bfb17bc344c13e81b287bb912587faf46669b755
Qwintal <mr.ankituniyal@gmail.com>
2023-09-13 09:06:35 +0530

    1.Exericise(Variable) This exercise helps to understand variable.

---
+++ 1.Exercise(Variable).py
@@ -0,0 +1,14 @@
+# We check in a patient name John Smith. He's 20 years old and is a new patient.
+# Define 3 variable "name, age and if he is a new patient."
+
+Full_name = "John Smith"
+Age = 20
+is_new_patient = True
+
+print(Full_name)
+print(Age)
+
+if is_new_patient:
+    print("Yes new patient")
+
+# In this exercise I only had to give variable and not print anything.
