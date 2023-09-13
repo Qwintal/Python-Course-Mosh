42ef5d232ddcb669a45bbf98317e50de00fda4c7
Qwintal <144579285+Qwintal@users.noreply.github.com>
2023-09-13 10:26:24 +0530

    Delete 2.Exericse(Input).py

--- 2.Exericse(Input).py
+++
@@ -1,6 +0,0 @@
-# Ask two questions: person's name and favourite color.Then, print a message like "mosh likes blue".
-Name = input("What is your name?")
-Favourite_color = input("What is your favourite color?")
-
-print(f"{Name} likes {Favourite_color}")
-
