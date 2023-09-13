541aa5ecfdf43e63f974fdc42dc756460a258e52
Qwintal <mr.ankituniyal@gmail.com>
2023-09-13 11:16:42 +0530

    3.Exercise(Conversion) This help in converting value, to perform artimatic calculation.

---
+++ 3.Exericse(Conversion).py
@@ -0,0 +1,10 @@
+# Ask a user their weight(in pounds), covert it to kilograms and print on terminal.
+Weight_pounds = int(input("What is your weight?(pounds)"))
+Weight_kilograms = Weight_pounds * 0.45
+
+print(f"Your weight in kilograms is {Weight_kilograms}")
+
+# You can use int or float (Float and integers can multiple with each other), but input functions give value in string
+# ,and you cannot multiply int/float with string.
+
+# You can put int/float function in line 2 or line 3 doesn't matter in this code.
