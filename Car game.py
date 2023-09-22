567a5c1413069dab79f19a3caea8ab47fb50cc0f
Qwintal <mr.ankituniyal@gmail.com>
2023-09-22 13:12:41 +0530

    Car game This Car game helps in revising everything I have learned so far.

---
+++ Car game.py
@@ -0,0 +1,29 @@
+# ">" arrow where user inputs, start-start they car, stop-stop the car, quit - quit game and help to display commands.
+# if user put anything instead of these 3 tell "I don't understand"
+Command = ""
+Started = False
+while True:
+    Command = input(">").lower()
+    if Command == "start":
+        if Started:
+            print("Car is already started!")
+        else:
+            Started = True
+            print("Car started...")
+    elif Command == "stop":
+        if not Started:
+            print("Car is already stopped!")
+        else:
+            Started = False
+            print("Car stopped...")
+    elif Command == "help":
+        print('''
+ start - to start car
+ stop - to stop car
+ quit - to quit    
+        ''')
+    elif Command == "quit":
+        break
+
+    else:
+        print("I Don't understand.")
