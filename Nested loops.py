0e42c678a1ee390782f88c56278540dcba4a1d97
Qwintal <mr.ankituniyal@gmail.com>
2023-09-22 15:13:59 +0530

    Helps in understanding nested loops.

--- Nested loops.py
+++ Nested loops.py
@@ -1,5 +1,9 @@
-for x in range(4):
-   for y in range(3):
-    print(f"({x}),({y})")
+number = [5, 2, 5, 2, 2]
+for x_count in number:
+    output = ""
+    for count in range(x_count):
+        output += "x"
+
+    print(output)
 
 # Nested loop means loop inside of a loop.
