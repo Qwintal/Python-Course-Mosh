48db1896a801e95b44c70ae3303cd0750f1a0e24
Qwintal <mr.ankituniyal@gmail.com>
2023-10-05 13:17:06 +0530

    This exercise helps in understand Classes, User_Define_Function AND Constructors.

---
+++ Classes, User-Define Function, Constructors.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f"Hi, I am {self.name}")
+
+
+john = Person("John Smith")
+john.talk()
+
+ankit = Person("Ankit Smith")
+ankit.talk()
